46171b6e4f4810447fa8e600d03da76c18892246
HuynhLoi <caoboiloi@users.noreply.github.com>
2020-05-27 10:15:23 +0700

    admin login

---
+++ views/admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../Content/Styles/admin.css" rel='stylesheet' type='text/css' media="all" />
+    <title>Buffalo Drive - Admin Login</title>
+</head>
+<body>
+<form class="form-3">
+    <p class="clearfix">
+        <label for="login">Username</label>
+        <input type="text" name="login" id="login" placeholder="Username">
+    </p>
+    <p class="clearfix">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Password">
+    </p>
+    <p class="clearfix">
+        <input type="submit" name="submit" value="Sign in">
+    </p>
+</form>
+<p class="copyright">© 2020 Official Signup Form. All Rights Reserved.</p>
+</body>
+</html>
